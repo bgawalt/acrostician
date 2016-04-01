3c54cc5a83e0c406b202bdbd937a75766aea96d9
Brian Gawalt <bgawalt@gmail.com>
2016-04-01 00:03:58 -0700

    Style clean up. I'm sorry, but penmanship counts.

--- acrostician.py
+++ acrostician.py
@@ -1,26 +1,31 @@
-import tweepy, sys, sqlite3, random
+import random
+import sqlite3
+import sys
+import tweepy
 
 from tweepy.streaming import StreamListener
 from tweepy import Stream
 from tweepy.api import API
 
-def getConfig(configFile):
+
+def get_config(config_file):
     """
-    :param configFile: Path to configuration file containing Twitter API keys
+    :param config_file: Path to configuration file containing Twitter API keys
     :return: Dictionary of configuration variables, including Twitter API keys
     """
-    with open(configFile, 'r') as config:
-        splitLines = [line.split("=") for line in config.xreadlines()]
+    with open(config_file, 'r') as config_stream:
+        split_lines = [line.split("=") for line in config_stream.xreadlines()]
         out = {}
-        for s in splitLines:
+        for s in split_lines:
             if len(s) == 2:
                 out[s[0].strip()] = s[1].strip()
         return out
 
+
 class StoreStatusTextListener(StreamListener):
     """ Records the text of streamed-in statuses in a list field """
     
-    def __init__(self, api, limit= 5):
+    def __init__(self, api, limit=5):
         self.texts = set()
         self.myLimit = limit
         self.api = api or API()
@@ -35,10 +40,11 @@ class StoreStatusTextListener(StreamListener):
 
 def subseqs(s):
     for a in xrange(len(s)):
-        for b in xrange(a+1,len(s)+1):
+        for b in xrange(a+1, len(s)+1):
             yield s[a:b]
 
-def firstChar(word):
+
+def first_char(word):
     if len(word) == 0:
         return ''
     if len(word) == 1:
@@ -47,25 +53,28 @@ def firstChar(word):
         return word[1]
     return word[0]
 
-def getNgrams(text):
-    cleanWords = [w.strip("""./?<>!,:;-+&*"'""") for w
-                          in text.lower().split()]
-    cleanNonemptyWords = [cw for cw in cleanWords if len(cw) > 0]
-    return subseqs(cleanNonemptyWords)
+
+def get_ngrams(text):
+    clean_words = [w.strip("""./?<>!,:;-+&*"'""") for w
+                   in text.lower().split()]
+    clean_nonempty_words = [cw for cw in clean_words if len(cw) > 0]
+    return subseqs(clean_nonempty_words)
+
 
 def initials(words):
-    firstChars = [firstChar(word).lower() for word in words if len(word) > 0]
-    return "".join(firstChars)
+    first_chars = [first_char(word).lower() for word in words if len(word) > 0]
+    return "".join(first_chars)
 
-def scrapeTwitter(acrostic, api, dbpath):
+
+def scrape_twitter(acrostic, api, dbpath):
     
-    subAcros = set(subseqs(acrostic))
-    acrosN = len(acrostic)
+    sub_acros = set(subseqs(acrostic))
+    acros_n = len(acrostic)
     
-    l = StoreStatusTextListener(api, limit = 10000)
+    l = StoreStatusTextListener(api, limit=10000)
     try:
         stream = Stream(auth, l, timeout=5.0)
-        stream.filter(track =  ("a","of","the","it","i"), languages=("en",))
+        stream.filter(track=("a", "of", "the", "it", "i"), languages=("en",))
     except:
         print "Problem after", len(l.texts)
   
@@ -73,47 +82,50 @@ def scrapeTwitter(acrostic, api, dbpath):
     with sqlite3.connect(dbpath+"/acrostician.db") as conn:
         cur = conn.cursor()
 
-        for n in xrange(1, acrosN+1):
+        for n in xrange(1, acros_n+1):
             s = str(n)
             cur.execute("create table if not exists GRAM_" + s +
-                " (Term text, Initials text, Count integer, Used integer);")
+                        " (Term text, Initials text, Count integer, "
+                        "Used integer);")
             cur.execute("create unique index if not exists IDX_TERM_GRAM_" + s +
                         " on GRAM_"+s+" (Term);")
             cur.execute("create index if not exists IDX_INIT_GRAM_" + s +
                         " on GRAM_"+s+" (Initials);")
             conn.commit()
 
-        ngramCounts = {}
+        ngram_counts = {}
         for text in l.texts:
-            ngrams = [s for s in getNgrams(text) if initials(s) in subAcros]
+            ngrams = [s for s in get_ngrams(text) if initials(s) in sub_acros]
             for n in ngrams:
                 term = " ".join(n)
                 if "http" in term.lower() or acrostic in term.lower():
                     continue
-                ngramCounts[n] = ngramCounts.get(n,0) + 1
+                ngram_counts[term] = ngram_counts.get(term, 0) + 1
         i = 1
-        for n in ngramCounts:
+        for term in ngram_counts:
+            n = term.split()
             inits = initials(n)
             cur.execute("select Count from GRAM_" + str(len(n)) +
-                            " where Term=:term", {"term": term})
+                        " where Term=:term", {"term": term})
             results = cur.fetchall()
             if len(results) == 0:
-                count = ngramCounts[n]
+                count = ngram_counts[term]
                 cur.execute("insert into GRAM_" + str(len(n)) +
-                                " values (?,?,?,1)", (term, inits, count))
+                            " values (?,?,?,1)", (term, inits, count))
             else:
-                count = ngramCounts[n] + results[0][0]
+                count = ngram_counts[term] + results[0][0]
                 cur.execute("update GRAM_" + str(len(n)) +
-                                " set Count=? where Term=?", (count, term))
+                            " set Count=? where Term=?", (count, term))
             if i % 100 == 0:
                 conn.commit()
-            i = i + 1
+            i += 1
         conn.commit()
 
-def capitalizeTweet(tw):
-    spTw = tw.split()
+
+def capitalize_tweet(tw):
+    sp_tw = tw.split()
     out = []
-    for w in spTw:
+    for w in sp_tw:
         if w[0] == "#":
             x = "#"+w[1:].capitalize()
         else:
@@ -121,7 +133,8 @@ def capitalizeTweet(tw):
         out.append(x)
     return "\n".join(out)
 
-def scoreTup(t):
+
+def score_tup(t):
     """
     Score an ngram tuple returned from a database ngram table. A higher scoring
     term is more deserving of inclusion in the resulting acrostic
@@ -139,111 +152,114 @@ def scoreTup(t):
         score = raw
     return max(score, 1)
 
-def postTweet(target, api, dbpath, testOnly = False):
-    targetLen = len(target)
+
+def post_tweet(target, api, dbpath, test_only=False):
+    target_len = len(target)
     with sqlite3.connect(dbpath+"/acrostician.db") as conn:
 
         print target
         cur = conn.cursor()
 
         cur.execute("create table if not exists TWEETED (Tweet text)")
-        cur.execute("create unique index if not exists IDX_TEXT "+
+        cur.execute("create unique index if not exists IDX_TEXT "
                     "on TWEETED(Tweet)")
 
-        notYetPosted = True
+        not_yet_posted = True
         tries = 100
 
-        while notYetPosted and tries > 0:
+        while not_yet_posted and tries > 0:
             tweet = ""
-            tweetWordCount = len([t for t in tweet.split() if len(t) > 0])
-            while tweetWordCount < targetLen:
-                desiredInits = target[tweetWordCount:targetLen]
+            tweet_word_count = len([t for t in tweet.split() if len(t) > 0])
+            while tweet_word_count < target_len:
+                desired_inits = target[tweet_word_count:target_len]
 
-                wordsLeft = targetLen - tweetWordCount
+                words_left = target_len - tweet_word_count
                 options = []
-                while len(options) < 200 and wordsLeft > 0:
-                    cur.execute("select Term, Initials, Count, Used from GRAM_"+
-                                str(wordsLeft)+" where Initials=? order by "+
+                while len(options) < 200 and words_left > 0:
+                    cur.execute("select Term, Initials, Count, Used "
+                                "from GRAM_" + str(words_left) +
+                                " where Initials=? order by " +
                                 "random() limit 200",
-                                (desiredInits[:wordsLeft],))
+                                (desired_inits[:words_left],))
                     for tup in cur.fetchall():
                         if target not in tup[0]:
                             options.append(tup)
-                    wordsLeft = wordsLeft - 1                
+                    words_left -= 1
                 if len(options) == 0:
-                    print "DAMMIT",tweet,desiredInits
+                    print "No options left", tweet, desired_inits
                     raise RuntimeError
-                totalCount = int(sum([scoreTup(o) for o in options]))
+                total_count = int(sum([score_tup(o) for o in options]))
 
-                r = random.randint(1, totalCount)
+                r = random.randint(1, total_count)
                 i = 0
                 while i < len(options):
-                    r = r - scoreTup(options[i])
+                    r = r - score_tup(options[i])
                     if r <= 0:
                         break
-                    i = i + 1
+                    i += 1
                 if r > 0:
-                    newWord = options[-1][0]
+                    new_word = options[-1][0]
                 else:
-                    newWord = options[i][0]
-                tweet = tweet + "\n"+newWord
-                tweetWordCount = len([t for t in tweet.split() if len(t) > 0])
+                    new_word = options[i][0]
+                tweet = tweet + "\n"+new_word
+                tweet_word_count = len([t for t in tweet.split() if len(t) > 0])
 
-            capTweet = capitalizeTweet(tweet)
-            lengthCheck = len(capTweet) < 141 # Check the length's okay
+            cap_tweet = capitalize_tweet(tweet)
+            length_check = len(cap_tweet) < 141  # Check the length's okay
 
-            dupeCheck = True  # Assume it's not a dupe...
-            for row in cur.execute("select Tweet from TWEETED where Tweet = ?",
-                                    (capTweet.lower(),)):
+            dupe_check = True  # Assume it's not a dupe...
+            for _ in cur.execute("select Tweet from TWEETED where Tweet = ?",
+                                 (cap_tweet.lower(),)):
                 print "Duplicate... trying again"
-                dupeCheck = False #But if it is, don't post it
+                dupe_check = False  # But if it is, don't post it
                 break
 
-            if lengthCheck and dupeCheck:
+            if length_check and dupe_check:
                 # Post the poem
                 try:
-                    print capTweet
+                    print cap_tweet
                 except:
                     print "[Trouble printing tweet]"
-                api.update_status(capTweet)
-                notYetPosted = False
+                api.update_status(cap_tweet)
+                not_yet_posted = False
 
-                #Update the used counts for this poem's ngrams
-                selectedNgrams = getNgrams(tweet)
-                for ngram in selectedNgrams:
+                # Update the used counts for this poem's ngrams
+                selected_ngrams = get_ngrams(tweet)
+                for ngram in selected_ngrams:
                     n = len(ngram)
                     inits = initials(ngram)
                     term = " ".join(ngram)
-                    cur.execute("select Used from GRAM_"+str(n)+
-                        " where Term = ?", (term,))
+                    cur.execute("select Used from GRAM_"+str(n) +
+                                " where Term = ?", (term,))
                     rows = cur.fetchall()
-                    for tup in rows: # Should just be a single row...
-                        usedCount = tup[0]
-                        cur.execute("update GRAM_"+str(n)+" set Used = ? "+
-                            "where Term = ?", (usedCount+1, term))
+                    for tup in rows:  # Should just be a single row...
+                        used_count = tup[0]
+                        cur.execute("update GRAM_"+str(n)+" set Used = ? " +
+                                    "where Term = ?", (used_count+1, term))
                     if len(rows) == 0:
                         cur.execute("insert into GRAM_" + str(n) +
-                                " values (?,?,1,1)", (term, inits))
-                    if testOnly:
+                                    " values (?,?,1,1)", (term, inits))
+                    if test_only:
                         print "usage update for", term, inits
-                        #Add the tweet to our list of already-posted
+                        # Add the tweet to our list of already-posted
                         cur.execute("insert into TWEETED values (?)",
-                                    (capTweet.lower(),))
+                                    (cap_tweet.lower(),))
                     conn.commit()
 
-            tries = tries - 1
+            tries -= 1
         print tries, "tries left"
 
+
 def usage():
-    print ("Usage: python acrosticBot.py [config file] "+
-            "[acrostic word] [read or write]")
+    print ("Usage: python acrosticBot.py [config file] "
+           "[acrostic word] [read or write]")
 
 if __name__ == "__main__":
     if not (len(sys.argv) == 4 or len(sys.argv) == 5):
         usage()
         sys.exit(0)
 
-    config = getConfig(sys.argv[1])
+    config = get_config(sys.argv[1])
     ckey = config["CONSUMER_KEY"]
     csec = config["CONSUMER_SECRET"]
     akey = config["ACCESS_KEY"]
@@ -255,19 +271,17 @@ if __name__ == "__main__":
     configapi = tweepy.API(auth)
     testmode = "test" in sys.argv
 
-    if random.randint(1,69) == 69:
+    if random.randint(1, 69) == 69:
         targetWord = "benghazi"
     else:
         targetWord = sys.argv[2].lower().split()[0]
 
     if sys.argv[3] == 'read':
-        scrapeTwitter(targetWord, configapi, worddb)
+        scrape_twitter(targetWord, configapi, worddb)
     elif sys.argv[3] == 'write':
-        postTweet(targetWord, configapi, worddb, testmode)
+        post_tweet(targetWord, configapi, worddb, testmode)
     elif sys.argv[3] == 'both':
-        scrapeTwitter(targetWord, configapi, worddb)
-        postTweet(targetWord, configapi, worddb, testmode)
+        scrape_twitter(targetWord, configapi, worddb)
+        post_tweet(targetWord, configapi, worddb, testmode)
     else:
         usage()
-
-
